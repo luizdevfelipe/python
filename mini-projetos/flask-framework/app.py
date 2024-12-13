b68cef30abb6e6939c667113b9d8e9aa151e49ff
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-12-13 15:05:02 -0300

    utilizando loops com Jinja
    Após passar variáveis para os templates
     com estrutura composta é possível iterar sobre elas com uma sintaxe semelhante a do Python, fornecida pelo template engine Jinja

--- mini-projetos/flask-framework/app.py
+++ mini-projetos/flask-framework/app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        name = request.form.get("name", None)
-        return render_template("greet.html", name=name)
-    
-    return render_template("index.html")
-    
+SPORTS = ("Basketball", "Football", "Soccer", "Running")
+
+@app.route("/", methods=["GET"])
+def index():        
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/sport", methods=["POST"])
+def sport():
+    if not request.form.get("name"):
+        return render_template("error.html", error="Invalid Values")
+    for sport in request.form.getlist("sport"):
+        if sport not in SPORTS:
+            return render_template("error.html", error="Invalid Values")
+        
+    sport = request.form.getlist("sport")
+    name = request.form.get("name")
+    return render_template("sport.html", sports=sport, name=name)
