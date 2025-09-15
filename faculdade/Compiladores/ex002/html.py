99356db5000fbf0cf642740ec4e2f7605f15350c
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-14 22:24:00 -0300

    separação básica com erros a serem tratados

--- faculdade/Compiladores/ex002/html.py
+++ faculdade/Compiladores/ex002/html.py
@@ -8,14 +8,27 @@ pagina = """<!DOCTYPE html>
     <title>Document</title>
 </head>
 <body>
-    <h1>Olá, Mundo!</h1>
-    <p>Este é um parágrafo de exemplo.</p>
+    <h1 style="color: #ff0000; font-size: 24px;">Olá, Mundo!</h1>
+    <p>Este é um parágrafo de exemplo.<br></p>
+    <img src="imagem.jpg" alt="Imagem de exemplo">
+    <a href='https://www.example.com'>Link para Example.com sdkhjdfhkjfdshjsdfhjksdfhjksfdjkhsfkdhjdfkhjsdfkhjshjdfhjfdshdf</a>
 </body>
 </html>"""
 
-# (<[a-z1-6]+>)(.+)(<\/[a-z1-6]+>)
 tokens = re.split('\n', pagina)
 
 # Nível | Tag | Atributos (lista) | Styles (lista) | innerHTML (resumo).
 for token in tokens:
-    print(token)
+    nivel = len(re.findall(r'   ', token))
+    tag = re.findall(r'<([a-z1-6]+)|</([a-z1-6]+)>', token)
+    atributos = re.findall(r'(\w+)="([^"]+)"', token)
+    styles = re.findall(r'style="([^"]+)"', token)
+    innerHTML = re.findall(r'>(.+)<\/[a-z1-6]+>', token)[0][0:20] + '...' if re.findall(r'>(.+)<\/[a-z1-6]+>', token) else None
+    
+    print(
+        "Nível:", nivel, " | ",
+        "Tag:", tag, " | ",
+        "Atributos:", atributos, " | ",
+        "Styles:", styles, " | ",
+        "innerHTML:", innerHTML
+    )
